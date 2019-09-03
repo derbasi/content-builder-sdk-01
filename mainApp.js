4294525d4928113c7255386cdadb85e6ab4eda28
Sebastian Heilmeier <sebastian@Sebastians-MBP.home>
2019-09-03 09:39:21 +0200

    schön gemacht?

--- mainApp.js
+++ mainApp.js
@@ -19,7 +19,51 @@ let saveData = () => {
 
     sdk.setData(mapData, (data) => {
         // mapData = data;
-        let content = `%%[set @RowSet = LookupRows('yopede_products','SKU','${mapData.sku}') set @RowCount = RowCount(@RowSet) if @RowCount > 0 then set @row = Row(@RowSet, 1) set @SKU = Field(@row, 'SKU') set @Name = Field(@row, 'Name')  set @Description = Field(@row, 'Description') set @Image = Field(@row, 'Image') set @Link = Field(@row, 'Link') set @Price = Field(@row, 'Price') endif]%%SKU: %%=v(@SKU)=%% <br />Name: %%=v(@Name)=%% <br />Description: %%=v(@Description)=%% <br />Image: %%=v(@Image)=%% <br />Link: %%=v(@Link)=%% <br />Price: %%=v(@Price)=%%`;
+        let content = `%%[set @RowSet = LookupRows('yopede_products','SKU','${mapData.sku}') set @RowCount = RowCount(@RowSet) if @RowCount > 0 then set @row = Row(@RowSet, 1) set @SKU = Field(@row, 'SKU') set @Name = Field(@row, 'Name')  set @Description = Field(@row, 'Description') set @Image = Field(@row, 'Image') set @Link = Field(@row, 'Link') set @Price = Field(@row, 'Price') endif]%%
+<tr>
+  <td align="center" valign="top" >
+    <table align="left" border="0" cellpadding="0" cellspacing="0" width="100%">
+      <tr>
+        <!-- added padding here -->
+        <td class="content_padding" style="padding : 0px 0px 0px 0px; ">
+          <!-- end of comment -->
+          <table border="0" cellpadding="0" cellspacing="0" width="100%">
+            <tr> <!-- top slot -->
+              <td align="center" class="header" valign="top">
+                <table align="left" border="0" cellpadding="0" cellspacing="0" width="100%">
+                  <tbody>
+                      <tr>
+                        <td align="left" valign="top">
+                          <table cellspacing="0" cellpadding="0" style="width:100%">
+                            <tbody>
+                            <tr>
+                              <td class="responsive-td" valign="top" style="width: 100%;">
+                                <table cellpadding="0" cellspacing="0" width="100%" style="background-color: transparent; min-width: 100%; " class="slot-styling"><tr><td style="padding: 0px 15px; " class="slot-styling camarker-inner"><table cellpadding="0" cellspacing="0" width="100%" style="min-width: 100%; " class="stylingblock-content-wrapper"><tr><td style="padding: 10px; " class="stylingblock-content-wrapper camarker-inner"><table cellspacing="0" cellpadding="0" style="width: 100%;"><tr><td><table cellspacing="0" cellpadding="0" style="width: 100%;"><tr><td valign="top" class="responsive-td" style="width: 50%; padding-right: 5px;"><table cellpadding="0" cellspacing="0" width="100%" style="min-width: 100%; " class="stylingblock-content-wrapper"><tr><td class="stylingblock-content-wrapper camarker-inner"><table width="100%" cellspacing="0" cellpadding="0"><tr><td align="center"><img src="%%=v(@Image)=%%" width="850" style="display: block; padding: 0px; text-align: center; border: 0px solid transparent; height: auto; width: 100%;"></td></tr></table></td></tr></table></td><td valign="top" class="responsive-td" style="width: 50%; padding-left: 5px;"><table cellpadding="0" cellspacing="0" width="100%" style="min-width: 100%; " class="stylingblock-content-wrapper"><tr><td class="stylingblock-content-wrapper camarker-inner"><b>%%=v(@Name)=%%</b><br>
+<i>%%=v(@Description)=%% </i><br>
+<br>
+<br>
+<span style="font-size:18px;"><b>%%=v(@Price)=%%</b></span><br>
+&nbsp;</td></tr></table><table cellpadding="0" cellspacing="0" width="100%" style="background-color: transparent; min-width: 100%; " class="stylingblock-content-wrapper"><tr><td style="padding: 0px 0px 20px; " class="stylingblock-content-wrapper camarker-inner"><table align="left" bgcolor="#00a1f4" border="0" cellpadding="0" cellspacing="0">
+ 
+  <tr>
+   <td style="font-family: Arial; font-size: 16px; line-height: 19px; color: #ffffff; text-align:center; font-weight:normal; ">
+    <a alias="" conversion="false" data-linkto="other" href="%%=v(@Link)=%%" style="color:#ffffff;text-decoration:none;font-family: Arial; font-size: 16px; line-height: 19px; text-align:center; font-weight:normal; background-color: #00a1f4;border: 10px solid #00a1f4;display: block; white-space: nowrap; " target="_blank" title="">Get them now!</a></td></tr></table></td></tr></table></td></tr></table></td></tr></table></td></tr></table></td></tr></table>
+                              </td>
+                            </tr>
+                            </tbody>
+                          </table>
+                        </td>
+                      </tr>
+                  </tbody>
+                </table>
+              </td>
+            </tr>
+          </table>
+        </td>
+      </tr>
+    </table>
+  </td>
+</tr>`;
         let superContent = defaultContent;
 
 
