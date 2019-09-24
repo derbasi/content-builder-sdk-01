85918e3fb50dc5195cce128ac735778d263f1cf3
Stefan Brunnengräber <brunnebbudler@gmx.de>
2019-09-24 15:32:44 +0200

    add img

--- mainApp.js
+++ mainApp.js
@@ -10,7 +10,7 @@ let mapData = {
     sku: ''
 };
 
-let defaultContent = '<p><img src="https://image.s10.sfmc-content.com/lib/fe3b15707564047a7d1573/m/1/59a1e816-d94f-4672-888c-018b41d8db52.jpg" width="255" style="display: block; padding: 0px; text-align: center; border: 0px solid transparent; height: auto; width: 100%;"></p>';
+let defaultContent = '<img src="https://dj-gmaps-sfmc-content-nlock.herokuapp.com//dragIcon.png" style="display:block;margin-left:auto;margin-right:auto">';
 
 let saveData = () => {
     console.log('Saving data...');
